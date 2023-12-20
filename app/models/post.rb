36e4dbd56529d5a6054792fd6c5f635f134410db
Noel Bryant <noelsobryant@gmail.com>
2023-12-21 02:32:19 +0300

    return five most recent comments on post

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,8 @@ class Post < ApplicationRecord
   def update_user_posts_counter
     user&.update(posts_counter: user.posts.count)
   end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
