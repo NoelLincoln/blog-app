1eecc2121c9a800488eade6b14fdfde78422d5d0
Noel Bryant <noelsobryant@gmail.com>
2023-12-21 02:38:45 +0300

    add functionality to update comments counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
+# app/models/comment.rb
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :update_post_comments_counter
+
+  private
+
+  def update_post_comments_counter
+    post&.update(comments_counter: post.comments.count)
+  end
 end
