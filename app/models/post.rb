fd4406c9d9239bcc5c1f8afcadefa026e275df60
Noel Bryant <noelsobryant@gmail.com>
2023-12-21 02:29:04 +0300

    add associations and implement posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  after_create :update_user_posts_counter
+
+  private
+
+  def update_user_posts_counter
+    user&.update(posts_counter: user.posts.count)
+  end
 end
