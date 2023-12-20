8ca9925237f0a168e09337122a8eb998c9f84351
Noel Bryant <noelsobryant@gmail.com>
2023-12-21 00:20:55 +0300

    rename post_counter column

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_20_084610) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "post_counter", default: 0
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
