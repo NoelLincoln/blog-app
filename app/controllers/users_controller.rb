b233654d0399c4ba2fa9755b33467bf38cef9317
Noel Bryant <noelsobryant@gmail.com>
2024-01-10 14:14:48 +0300

    add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    @posts = @user.posts
+  end
+end
