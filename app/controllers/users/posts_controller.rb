67d84a791d29d9159679c25cc3cbdbb60b4eb048
Noel Bryant <noelsobryant@gmail.com>
2024-01-10 14:16:18 +0300

    add controller to get posts by a user

---
+++ app/controllers/users/posts_controller.rb
@@ -0,0 +1,20 @@
+# app/controllers/users/posts_controller.rb
+module Users
+  class PostsController < ApplicationController
+    before_action :set_user, only: [:index, :show]
+
+    def index
+      @posts = @user.posts
+    end
+
+    def show
+      @post = @user.posts.find(params[:id])
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+  end
+end
