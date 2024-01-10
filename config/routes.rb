cabe91a9deae0480b436c108ac4b32f39db53277
Noel Bryant <noelsobryant@gmail.com>
2024-01-11 01:33:02 +0300

    remove duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get "users/:id" => "users#show", as: :user
-  get "users/:user_id/posts" => "users/posts#index", as: :user_posts
-  get "users/:user_id/posts/:id" => "users/posts#show", as: :user_post
-
   resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :show]
   end
