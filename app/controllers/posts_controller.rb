314869aa59288db1e48e78db9ab768ab276c5375
Noel Bryant <noelsobryant@gmail.com>
2024-01-10 14:15:21 +0300

    add posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+# app/controllers/posts_controller.rb
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+end
