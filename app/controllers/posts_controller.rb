62e7e2dadae7c1f8bb8740e3be7405f7b560f130
Noel Bryant <noelsobryant@gmail.com>
2024-01-10 16:47:35 +0300

    update posts controller to get user posts and make code DRY

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,18 @@
 # app/controllers/posts_controller.rb
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
+    before_action :set_user, only: %i[index show]
 
-  def show
-    @post = Post.find(params[:id])
-  end
+    def index
+      @posts = @user.posts
+    end
+
+    def show
+      @post = @user.posts.find(params[:id])
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
 end
