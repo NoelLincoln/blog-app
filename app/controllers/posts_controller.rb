4ed9f1e1d01315f59bb23524438c80a17a7dd124
Noel Bryant <noelsobryant@gmail.com>
2024-01-12 03:59:09 +0300

    query data from backend and add pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,14 @@ class PostsController < ApplicationController
   before_action :set_user, only: %i[index show]
 
   def index
-    @posts = @user.posts
+    @user = User.find_by_id(params[:user_id])
+    @posts = @user.posts.paginate(page: params[:page], per_page: 2)
   end
 
   def show
     @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+    @comments_count = @comments.size
   end
 
   private
