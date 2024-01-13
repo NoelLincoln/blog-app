b5070eaff03fe157551829ccfdaa3aa1fe0b3a09
Noel Bryant <noelsobryant@gmail.com>
2024-01-13 15:31:56 +0300

    create actions for creating a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/posts_controller.rb
 class PostsController < ApplicationController
-  before_action :set_user, only: %i[index show]
+  before_action :set_user, only: %i[index show new create]
 
   def index
     @user = User.find_by_id(params[:user_id])
@@ -13,9 +13,33 @@ class PostsController < ApplicationController
     @comments_count = @comments.size
   end
 
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to user_posts_path(@user)
+    else
+      flash.now[:error] = 'Error: Post could not be saved'
+      puts "Errors: #{@post.errors.full_messages}"
+      render :new
+    end
+  end
+
+
+
   private
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
