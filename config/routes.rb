6b92769c6e07486c6d47b61beb48a6f10f4725e6
Noel Bryant <noelsobryant@gmail.com>
2024-01-13 19:31:22 +0300

    add partials and implement like feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
-      resources :likes, only: [:new, :create]
-    end
+        resources :comments, only: [:new, :create]
+        resources :likes, only: [:new, :create]
+      end
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
