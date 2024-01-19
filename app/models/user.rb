5cc8733093c3ae035e7d35dd1a12c97aa00783c7
Noel Bryant <noelsobryant@gmail.com>
2024-01-20 01:04:08 +0300

    add devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 # app/models/user.rb
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :comments
   has_many :posts, foreign_key: 'author_id'
   has_many :likes
