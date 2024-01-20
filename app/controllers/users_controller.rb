c5a4c97b4dc1b00fde09ffaa96301a2b6b7c3f9d
Noel Bryant <noelsobryant@gmail.com>
2024-01-20 16:07:48 +0300

    generate token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
     @user = User.includes(posts: :comments).find_by_id(params[:id])
     @recent_posts = @user.recent_posts
   end
+
+  def generate_token
+    @user = User.find(current_user.id)
+    @user.generate_token
+    if @user.save
+      flash[:success] = 'Token generated successfully'
+    else
+      flash[:error] = @user.errors.full_messages.join(', ')
+    end
+    redirect_to user_path(@user)
+  end
 end
