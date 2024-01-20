771b436cc459fb87194503c047745ad2060f4880
Noel Bryant <noelsobryant@gmail.com>
2024-01-20 15:12:29 +0300

    add destroy action for posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
         resources :comments, only: [:new, :create, :destroy]
-        resources :likes, only: [:new, :create, :destroy]
+        resources :likes, only: [:new, :create]
       end
   end
 
