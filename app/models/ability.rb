0b16d4f7ce5e98995443d3fbf8cf6bd6820f04aa
Noel Bryant <noelsobryant@gmail.com>
2024-01-20 15:14:31 +0300

    fix redirect after deleting comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
       can :manage, :all
     else
       can :read, :all
-      can %i[destroy create], Comment, author_id: user.id
+      can %i[destroy create], Comment, user_id: user.id
       can %i[destroy create], Post, author_id: user.id
     end
   end
