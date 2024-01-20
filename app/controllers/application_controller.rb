f40efb6f39a12746714c1d2864883b1211bcdc70
Noel Bryant <noelsobryant@gmail.com>
2024-01-20 16:07:11 +0300

    format response to json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :json_request?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :null_session, if: :json_request?
+
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
   end
 
   protected
+
+  def json_request?
+    request.format.json?
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation bio])
