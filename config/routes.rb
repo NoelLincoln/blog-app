9db1278dfe468cd00a5498d101edbfd2a9151c1a
Noel Bryant <noelsobryant@gmail.com>
2024-01-20 16:05:44 +0300

    write api endpoint routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,18 @@ Rails.application.routes.draw do
       end
   end
 
+  post "users/:id/generate_token" => "users#generate_token", as: :generate_token
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index, :create] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
+
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
