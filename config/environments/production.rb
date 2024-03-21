60a5349aca2c9f7807d1677d7dc6d9e976d6f271
Noel Bryant <noelsobryant@gmail.com>
2024-03-21 14:34:58 +0300

    set mail sender after successful registration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,22 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "blog_app_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
 
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => "nnoelsobryant@gmail.com",
+    :password             => "jhkatchxulphtosx",
+    :authentication       => "plain",
+    # :ssl                  => true,
+    # :tsl                  => true,
+    :enable_starttls_auto => true
+
+    # :enable_starttls_auto => true # I don't have this, but it should work anyway
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
